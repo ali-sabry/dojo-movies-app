f19f4becad76e3cc9f2a0974b767d4f1fc9a946a
ali-sabry <th3man1998@gmail.com>
2022-10-12 20:58:00 +0200

    Fix Issues

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,21 +3,20 @@ import React from 'react';
 import classes from './styles/Style.module.css';
 
 import useFetchingApi from '../global/FetchingApi';
-// import MoviesCards from '../global/MoviesCards';
-// import CustomCard from '../global/CustomCard';
+import MoviesCards from '../global/MoviesCards';
+import CustomCard from '../global/CustomCard';
 
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 
 const Home = ()=> {
     const url ='https://api.themoviedb.org/3/movie/now_playing';
-
-    const {IsLoading, IsError, MessageError, MoviesData: NowPlaying} = useFetchingApi(url);
-    console.log(NowPlaying);
-    // const {MoviesData: Popular}  = useFetchingApi('https://api.themoviedb.org/3/movie/popular');
-    // const {MoviesData: Upcoming} = useFetchingApi('https://api.themoviedb.org/3/movie/upcoming');
-    // const {MoviesData: Toprated} = useFetchingApi('https://api.themoviedb.org/3/movie/top_rated');
-    // const {MoviesData: Trending} = useFetchingApi('https://api.themoviedb.org/3/trending/movie/week');
+    const {IsLoading, IsError, MessageError, MoviesData} = useFetchingApi(url);
+    
+    const {MoviesData: Popular}  = useFetchingApi('https://api.themoviedb.org/3/movie/popular');
+    const {MoviesData: Upcoming} = useFetchingApi('https://api.themoviedb.org/3/movie/upcoming');
+    const {MoviesData: Toprated} = useFetchingApi('https://api.themoviedb.org/3/movie/top_rated');
+    const {MoviesData: Trending} = useFetchingApi('https://api.themoviedb.org/3/trending/movie/week');
     
     if(IsLoading) {
         return (
@@ -34,97 +33,90 @@ const Home = ()=> {
             </>
         )
     };
-    
-    const ShowData = ({ Movies, type = '', pagePath, easing = 'cubic-bezier(.17,.67,.83,.67)' }) => {
-        return (
-            <Splide className={classes.slider} options={{
-                perPage : 3,
-                pagination: false,
-                type: type,
-                gap: '2rem',
-                easing: easing,
-                lazyLoad: 'nearby',
-                breakpoints: {
-                    576: {
-                        perPage: 1,
-                    }
-                } 
-            }}>
-                {
-                    console.log(NowPlaying)
-                    // Movies && (
-                    //     Movies.map((mv, index)=> (
-                    //         index < 10 ? (
-                    //             <SplideSlide key={`key_${index}`}>
-                    //                 <MoviesCards movies={mv} />
-                    //             </SplideSlide>
-                    //         ) : false
-                    //     )) 
-                    // )
-                }
-                <SplideSlide>
-                {
-                     console.log(NowPlaying)
-                }
-                    {/*<CustomCard pagePath={pagePath} />*/}
-                </SplideSlide>
-            </Splide>
-        )
-    };
 
+    console.log(MoviesData, Popular, Upcoming, Toprated, Trending);
+    
     return (
-        <section className={classes.HomePage} style={{paddingBottom: '50px'}}>
-            <div className={classes.Nowplaying_Movies}> 
-                <h1 className={classes.Home_Heading}>now playing movies</h1>
-                    <Splide className={classes.slider} 
-                        options={{
-                        perPage : 3,
-                        pagination: false,
-                        type: 'loop',
-                        autoplay: true,
-                        gap: '1rem',
-                        height: 'auto',
-                        easing: 'cubic-bezier(.17,.67,.83,.67)',
-                        lazyLoad: 'nearby',
-                        breakpoints: {
-                            640: {
-                            perPage: 1,
-                            }
-                        } 
-                     }}>
-                {
-                    console.log(NowPlaying)
-                    // (
-                    //     MoviesData.map( (movie, index) => (
-                    //         <>
-                    //             {console.log(MoviesData)}
-                    //             <SplideSlide key={index}>
-                    //                 <MoviesCards movies={movie} key={index} />
-                    //             </SplideSlide>
-                    //         </>
-                    //     ))
-                    // )
-                }
-                </Splide>
-            </div>
-            <div className={classes.Popular_Slides}>
-                <h1 className={classes.Home_Heading}>popular movies</h1>
-                {/*<ShowData Movies={ Popular } pagePath='/Popular' />*/}
-            </div>
-            <div className={classes.Upcoming_Slides}>
-                <h1 className={classes.Home_Heading}>upcoming movies</h1>
-                {/*<ShowData Movies={ Upcoming } pagePath='/Upcoming' />*/}
-            </div>
-            <div className={classes.Toprated_Slides}>
-                <h1 className={classes.Home_Heading}>Toprated movies</h1>
-                {/*<ShowData Movies={ Toprated } pagePath='/Toprated'  />*/}
-            </div>
-            <div className={classes.Trending_Slides}>
-                <h1 className={classes.Home_Heading}>Trending movies</h1>
-                {/*<ShowData Movies={ Trending } pagePath='/Trending'  />*/}
-            </div>
-        </section>
+        <h1>home page</h1>
     )
+
+    // const ShowData = ({ Movies, type = '', pagePath, easing = 'cubic-bezier(.17,.67,.83,.67)' }) => {
+    //     return (
+    //         <Splide className={classes.slider} options={{
+    //             perPage : 3,
+    //             pagination: false,
+    //             type: type,
+    //             gap: '2rem',
+    //             easing: easing,
+    //             lazyLoad: 'nearby',
+    //             breakpoints: {
+    //                 576: {
+    //                     perPage: 1,
+    //                 }
+    //             } 
+    //         }}>
+    //             {
+    //                 Movies.map((mv, index)=> (
+    //                     index < 10 ? (
+    //                         <SplideSlide key={`key_${index}`}>
+    //                             <MoviesCards movies={mv} />
+    //                         </SplideSlide>
+    //                     ) : false
+    //                 )) 
+                    
+    //             }
+    //             <SplideSlide>
+    //                 <CustomCard pagePath={pagePath} />
+    //             </SplideSlide>
+    //         </Splide>
+    //     )
+    // };
+
+    // return (
+    //     <section className={classes.HomePage} style={{paddingBottom: '50px'}}>
+    //         <div className={classes.Nowplaying_Movies}> 
+    //             <h1 className={classes.Home_Heading}>now playing movies</h1>
+    //                 <Splide className={classes.slider} 
+    //                     options={{
+    //                     perPage : 3,
+    //                     pagination: false,
+    //                     type: 'loop',
+    //                     autoplay: true,
+    //                     gap: '1rem',
+    //                     height: 'auto',
+    //                     easing: 'cubic-bezier(.17,.67,.83,.67)',
+    //                     lazyLoad: 'nearby',
+    //                     breakpoints: {
+    //                         640: {
+    //                         perPage: 1,
+    //                         }
+    //                     } 
+    //                  }}>
+    //             {MoviesData.map( (movie, index) => (
+    //                 <SplideSlide key={index}>
+    //                     <MoviesCards movies={movie} key={index} />
+    //                 </SplideSlide>
+    //             ))}
+    //             </Splide>
+    //         </div>
+    //         <div className={classes.Popular_Slides}>
+    //             <h1 className={classes.Home_Heading}>popular movies</h1>
+    //             <ShowData Movies={ Popular } pagePath='/Popular' />
+    //         </div>
+    //         <div className={classes.Upcoming_Slides}>
+    //             <h1 className={classes.Home_Heading}>upcoming movies</h1>
+    //             <ShowData Movies={ Upcoming } pagePath='/Upcoming' />
+    //         </div>
+    //         <div className={classes.Toprated_Slides}>
+    //             <h1 className={classes.Home_Heading}>Toprated movies</h1>
+    //             <ShowData Movies={ Toprated } pagePath='/Toprated'  />
+    //         </div>
+    //         <div className={classes.Trending_Slides}>
+    //             <h1 className={classes.Home_Heading}>Trending movies</h1>
+    //             <ShowData Movies={ Trending } pagePath='/Trending'  />
+    //         </div>
+    //     </section>
+    // )
 };
 
 export default Home;

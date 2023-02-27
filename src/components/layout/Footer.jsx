7c1ef9eff4081b7f9379be2fac6a33385f1e22a9
Ali Sabry <alisabry.official@gmail.com>
2023-02-27 15:54:23 +0000

    Fixed Two Issues
    1. Make the footer copyright date updated dynamic using JavaScript Date Function
    2. Fix the recommendations boxes layout

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import classes from '../../styles/Footer.module.css';
 const Footer = () => {
     return (
         <footer className={classes.Footer}>
-            <p>all copy right resarved &copy; 2022 made by ❤
+            <p>all copy right resarved &copy; {new Date().getFullYear()} made by ❤
                 <span><a target={'_blank' && {}} href={"https://www.linkedin.com/in/ali-sabry"}>ali sabry</a></span>
                 ❤
             </p>
